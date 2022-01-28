9c6f6d471521c7bbffec859c8b4aba01475a1c5e
adrianlm2 <adrianl0@chicagobooth.edu>
2022-01-28 20:07:01 +0000

    updating

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,8 @@ class ApplicationController < ActionController::Base
     render({ :template => "calculation_templates/square_form.html.erb"})
   end 
   def calculate_square
+    @num = params.fetch("square_input").to_i
+    @num_squared = @num ** 2
     render({:template => "calculation_templates/square_results.html.erb"})
   end
 
